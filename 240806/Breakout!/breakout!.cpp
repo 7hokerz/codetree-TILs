b891c1450eea588ce95b5dbf531c20943f7f29c9
7hokerz <77675374+7hokerz@users.noreply.github.com>
2024-08-06 20:20:59 +0900

    Title: Breakout!, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/Breakout!/breakout!.cpp
+++ 240806/Breakout!/breakout!.cpp
@@ -7,10 +7,9 @@ int main(){ cin.tie(0)->sync_with_stdio(0); cout.tie(0);
     cin >> N;
     for(int i = 1;i<N+1;i++){
         cin >> arr[i];
-        if(arr[i] != -1){
+        if(arr[i] != -1 && arr[i-1] == -1){
             int j = i - 1;
-            while(1){
-                if(arr[j] != -1 || j < 1){ cout << -1; return 0;}
+            while(arr[j] == -1 && j >= 1){
                 arr[j] = arr[j + 1] - 1;
                 if(!arr[j]) break;
                 j--;
@@ -18,19 +17,28 @@ int main(){ cin.tie(0)->sync_with_stdio(0); cout.tie(0);
         }
     }
 
-    bool chk = false;
-
     for(int i = 1;i<N+1;i++){
         if(arr[i] == -1 || !arr[i]) mx++;
 
-        if(arr[i] == -1 && !chk){
-            mn++; chk = true;
-        }
-        else if(!arr[i]){
-            chk = false;
-            mn++;
+        if(i == 1 && arr[i] == -1) arr[i] = 0;
+        else if(arr[i] == -1 && arr[i-1] != -1){
+            arr[i] = arr[i-1] + 1;
         }
         //cout << arr[i] << ' ';
     }//cout << '\n';
+
+    bool chk = true;
+
+    for(int i = 1;i<N+1;i++) {
+        if(chk){
+           if(arr[i]){cout << -1; return 0;} 
+           else chk = false;
+        }
+        else{
+            if(i != N && !arr[i+1]) chk = true;
+        }
+
+        if(!arr[i]) mn++;
+    }
     cout << mn << ' ' << mx;
 }
